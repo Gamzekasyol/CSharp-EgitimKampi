cd123c5125faa541a0560b20dfa888c414410fee
Gamze <101417422+Gamzekasyol@users.noreply.github.com>
2024-11-17 01:27:17 +0300

    Yerel değişiklikleri kaydediyorum

--- 08_Methods/obj/Debug/net8.0/08_Methods.AssemblyInfo.cs
+++ 08_Methods/obj/Debug/net8.0/08_Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("08_Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b63001462b0925749f4dcece8291d64ac0a0709")]
 [assembly: System.Reflection.AssemblyProductAttribute("08_Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("08_Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  *** C# Veri Tabanlı Ürün-Kategori Bilgi Listesi ***");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("------------------------");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source=Gamze\\SQLEXPRESS;Initial Catalog=Egitim_KampiDb;Integrated Security=True"); ;
+
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Select * From Table_Category", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    foreach(var item in row.ItemArray)
+                    {
+                        Console.Write(item.ToString());
+                    }
+                    Console.WriteLine();
+                        
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bağlantı hatası oluştu!", ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
